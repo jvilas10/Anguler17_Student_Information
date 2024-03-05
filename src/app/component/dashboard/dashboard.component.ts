ef4e0a1c796a63ff90ecbf2c4ef36de3517b2df5
vilas <jvilas10@gmail.com>
2024-03-05 11:39:46 +0530

    My First Push

---
+++ src/app/component/dashboard/dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'student-information-dashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.scss'
+})
+export class DashboardComponent {
+
+}
